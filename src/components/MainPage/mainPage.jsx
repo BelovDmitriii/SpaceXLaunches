7708e501596662b5f0c590d42019370a6b5d744c
Belov Dmitrii <belovd90@mail.ru>
2023-08-02 01:43:52 +0300

    added filter and sort

--- src/components/MainPage/mainPage.jsx
+++ src/components/MainPage/mainPage.jsx
@@ -13,29 +13,34 @@ function SpaceXLaunches() {
       .catch(error => console.log(error));
   }, []);
 
-  const sortLaunches = () => {
-    if (sortOrder === "descending") {
-      setLaunches(launches.sort((a, b) => new Date(b.date_utc) - new Date(a.date_utc)));
-      setSortOrder("ascending");
-    } else {
-      setLaunches(launches.sort((a, b) => new Date(a.date_utc) - new Date(b.date_utc)));
-      setSortOrder("descending");
-    }
+  const setSort = () => {
+    sortOrder === "descending" ? setSortOrder("ascending") : setSortOrder("descending");
   };
 
+  const handleSortChange = () => {
+    sortOrder === 'descending' ? 
+    setLaunches(launches.sort((a, b) => new Date(b.date_utc) - new Date(a.date_utc)))
+    : setLaunches(launches.sort((a, b) => new Date(a.date_utc) - new Date(b.date_utc)));
+    setSort();
+  }
+
   return (
-    <div>
-      <button onClick={sortLaunches}>{sortOrder === "descending" ? "Sort by Oldest" : "Sort by Newest"}</button>
+    <>
+      <button onClick={handleSortChange}>{sortOrder === "ascending" ? "Sort by Oldest" : "Sort by Newest"}</button>
       {launches.filter(item => Number(item.date_local.split('-')[0]) >= 2015 
-        && Number(item.date_local.split('-')[0]) <= 2019 && item.success).map(launch => (
+        && Number(item.date_local.split('-')[0]) <= 2019 && item.success).reverse().map(launch => (
           <div key={launch.id}>
-          <h2>{launch.mission_name}</h2>
-          <p>{new Date(launch.date_utc).toLocaleDateString()}</p>
-          <p>{launch.details}</p>
-          <img style={{width:"50%", height:"300px"}} src={launch.links.flickr.original[0]} alt={launch.rocket} />
-        </div>
-      ))}
-    </div>
+            <div>
+              <h2>{launch.mission_name}</h2>
+              <p>{new Date(launch.date_utc).toLocaleDateString()}</p>
+              <p>{launch.details}</p>
+            </div>
+          <div style={{height:"200px", width:"200px"}}>
+            <img style={{height:"90%"}} src={launch.links.flickr.original[launch.links.flickr.original.length - 1]} alt='rocket' />
+          </div>
+          </div>
+        ))}
+    </>
   );
 }
 
