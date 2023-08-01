a672080e9851fd95fc581073d3aa6866d256901d
Belov Dmitrii <belovd90@mail.ru>
2023-08-02 00:57:07 +0300

    some code refactored

--- src/components/MainPage/mainPage.jsx
+++ src/components/MainPage/mainPage.jsx
@@ -1,29 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const BASE_URL = 'https://api.spacexdata.com/v3/launches';
+const URL = 'https://api.spacexdata.com';
 
 function SpaceXLaunches() {
   const [launches, setLaunches] = useState([]);
   const [sortOrder, setSortOrder] = useState("descending");
 
   useEffect(() => {
-    axios.get(BASE_URL, {
-      params: {
-        start: "2015-01-01",
-        end: "2019-12-31",
-      },
-    })
+    axios.get(`${URL}/v5/launches`)
       .then(response => setLaunches(response.data))
       .catch(error => console.log(error));
   }, []);
 
   const sortLaunches = () => {
     if (sortOrder === "descending") {
-      setLaunches(launches.sort((a, b) => new Date(b.launch_date_utc) - new Date(a.launch_date_utc)));
+      setLaunches(launches.sort((a, b) => new Date(b.date_utc) - new Date(a.date_utc)));
       setSortOrder("ascending");
     } else {
-      setLaunches(launches.sort((a, b) => new Date(a.launch_date_utc) - new Date(b.launch_date_utc)));
+      setLaunches(launches.sort((a, b) => new Date(a.date_utc) - new Date(b.date_utc)));
       setSortOrder("descending");
     }
   };
@@ -31,12 +26,13 @@ function SpaceXLaunches() {
   return (
     <div>
       <button onClick={sortLaunches}>{sortOrder === "descending" ? "Sort by Oldest" : "Sort by Newest"}</button>
-      {launches.map(launch => (
+      {launches.filter(item => Number(item.date_local.split('-')[0]) >= 2015 
+        && Number(item.date_local.split('-')[0]) <= 2019 && item.success).map(launch => (
           <div key={launch.id}>
           <h2>{launch.mission_name}</h2>
-          <p>{new Date(launch.launch_date_utc).toLocaleDateString()}</p>
+          <p>{new Date(launch.date_utc).toLocaleDateString()}</p>
           <p>{launch.details}</p>
-          <img src={launch.rocket.img} alt={launch.rocket.rocket_name} />
+          <img style={{width:"50%", height:"300px"}} src={launch.links.flickr.original[0]} alt={launch.rocket} />
         </div>
       ))}
     </div>
