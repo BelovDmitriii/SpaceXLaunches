86d6728897e4b2b46fd53f1fe5cb15acfd475a61
Belov Dmitrii <belovd90@mail.ru>
2023-08-02 01:53:03 +0300

    some test styles added

--- src/components/MainPage/mainPage.jsx
+++ src/components/MainPage/mainPage.jsx
@@ -26,16 +26,16 @@ function SpaceXLaunches() {
 
   return (
     <>
-      <button onClick={handleSortChange}>{sortOrder === "ascending" ? "Sort by Oldest" : "Sort by Newest"}</button>
+      <button onClick={handleSortChange}>{sortOrder === "descending" ? "Sort by Oldest" : "Sort by Newest"}</button>
       {launches.filter(item => Number(item.date_local.split('-')[0]) >= 2015 
         && Number(item.date_local.split('-')[0]) <= 2019 && item.success).reverse().map(launch => (
-          <div key={launch.id}>
+          <div key={launch.id} style={{width:"60%", textAlign:"center", margin:"auto"}}>
             <div>
               <h2>{launch.mission_name}</h2>
               <p>{new Date(launch.date_utc).toLocaleDateString()}</p>
               <p>{launch.details}</p>
             </div>
-          <div style={{height:"200px", width:"200px"}}>
+          <div style={{height:"250px", width:"100%"}}>
             <img style={{height:"90%"}} src={launch.links.flickr.original[launch.links.flickr.original.length - 1]} alt='rocket' />
           </div>
           </div>
